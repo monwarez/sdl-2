ba6715f3902a3a5335b889321d85275e37598325
monwarez <monwarez@gmail.com>
2014-05-02 23:13:51 +0200

    Start a new part 	new file:   data/autoexec 	new file:   data/map1/objet 	modified:   engine/Makefile 	new file:   engine/RcManager.cpp 	new file:   engine/RcManager.h 	modified:   projet-freebsd

---
+++ engine/RcManager.h
@@ -0,0 +1,35 @@
+/*
+Copyright (C) 2014 Payet thibault
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+#ifndef RCMANAGER_H
+#define RCMANAGER_H
+#include "../graphic/Texture.h"
+#include "Object.h"
+
+// Normalement RCMANAGER devrait être un singleton , il en faut un seul par programme
+// Donc je n'en ferais qu'un seul that's all
+class	RcManager
+{
+	public:
+		RcManager();
+		~RcManager();
+
+	private:
+	
+	protected:
+	
+};
+#endif
